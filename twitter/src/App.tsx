bbbce00d8ef249b5b047f656b3518f1ec45d66d2
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-06 17:30:39 +0900

    fixed logo

--- twitter/src/App.tsx
+++ twitter/src/App.tsx
@@ -5,6 +5,9 @@ import Profile from "./routes/profile";
 import Login from "./routes/login";
 import CreateAccount from "./routes/create_account";
 
+// twitter blue verification user
+// https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Twitter_Verified_Badge.svg/1024px-Twitter_Verified_Badge.svg.png
+
 const router = createBrowserRouter([
     {
         path: "/",
--- twitter/src/routes/home.tsx
+++ twitter/src/routes/home.tsx
@@ -1,3 +1,13 @@
+import { Helmet } from "react-helmet";
+
 export default function Home() {
-    return <h1>Home sweet home~</h1>;
+    return (
+        <>
+            <Helmet>
+                <title>X. 무슨 일이 일어나고 있나요? / X</title>
+                {/* <link rel="icon" href="%PUBLIC_URL%/twitter_icon.ico" /> */}
+            </Helmet>
+            <h1>Home sweet home~</h1>
+        </>
+    );
 }
