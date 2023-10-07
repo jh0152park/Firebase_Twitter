d335de791a660f04d1689af1b8e44189f1e1d9a2
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-07 21:28:12 +0900

    created search button on sidebar

--- twitter/src/routes/feed_style/sidebar.tsx
+++ twitter/src/routes/feed_style/sidebar.tsx
@@ -1,38 +1,17 @@
 import { Box, Button, HStack, Heading, Text, VStack } from "@chakra-ui/react";
 import { useNavigate } from "react-router-dom";
-import { RiHome7Fill } from "react-icons/ri";
+import Logo from "./sidebar_style/logo";
+import HomeButton from "./sidebar_style/home_button";
+import SearchButton from "./sidebar_style/search_button";
 
 export default function Sidebar() {
     const navigate = useNavigate();
 
     return (
         <VStack alignItems={"flex-start"}>
-            <Box
-                fontSize="40px"
-                mt="5px"
-                pl="10px"
-                onClick={() => {
-                    navigate("/feed");
-                }}
-                _hover={{ cursor: "pointer" }}
-                mb="30px"
-            >
-                𝕏
-            </Box>
-
-            <HStack
-                fontSize="30px"
-                pl="10px"
-                pr="20px"
-                py="5px"
-                borderRadius="20px"
-                _hover={{ cursor: "pointer", bgColor: "whiteAlpha.300" }}
-            >
-                <RiHome7Fill />
-                <Text fontSize="25px" fontWeight="bold" ml="10px">
-                    홈
-                </Text>
-            </HStack>
+            <Logo />
+            <HomeButton />
+            <SearchButton />
         </VStack>
     );
 }
