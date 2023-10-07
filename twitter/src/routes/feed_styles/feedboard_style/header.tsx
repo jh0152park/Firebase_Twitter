ea02e83b9ebb68c3271da731e26282aaea364fd5
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-08 01:00:59 +0900

    created header buttons

--- twitter/src/routes/feed_styles/feedboard_style/header.tsx
+++ twitter/src/routes/feed_styles/feedboard_style/header.tsx
@@ -1,6 +1,9 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Container, HStack, VStack } from "@chakra-ui/react";
+import { useState } from "react";
 
 export default function Header() {
+    const [recommend, setRecommend] = useState(true);
+
     return (
         <Box
             w="598px"
@@ -8,8 +11,86 @@ export default function Header() {
             position="fixed"
             top="0"
             borderBottom="1px"
-            opacity="0.5"
+            borderColor="rgba(0, 0, 0, 2)"
             bgColor="rgba(0, 0, 0, 0.7)"
-        ></Box>
+        >
+            <VStack alignItems="flex-start" h="100%">
+                <Box
+                    h="50%"
+                    w="100%"
+                    fontSize="20px"
+                    fontWeight="bold"
+                    color="white"
+                    pt="10px"
+                    pl="10px"
+                >
+                    홈
+                </Box>
+                <HStack h="50%" w="100%" spacing="0" mb="0" pb="0">
+                    <Container
+                        h="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        _hover={{
+                            bgColor: "rgba(255, 255, 255, 0.1)",
+                            cursor: "pointer",
+                        }}
+                        fontSize="15px"
+                        fontWeight="bold"
+                    >
+                        <Box
+                            p="15px"
+                            borderBottom="4px"
+                            borderColor={
+                                recommend
+                                    ? "twitter.500"
+                                    : "rgba(255, 255, 255, 0)"
+                            }
+                            color={
+                                recommend ? "white" : "rgba(255, 255, 255, 0.4)"
+                            }
+                            onClick={() => {
+                                setRecommend(true);
+                            }}
+                        >
+                            추천
+                        </Box>
+                    </Container>
+                    <Container
+                        h="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        _hover={{
+                            bgColor: "rgba(255, 255, 255, 0.1)",
+                            cursor: "pointer",
+                        }}
+                        fontSize="15px"
+                        fontWeight="bold"
+                    >
+                        <Box
+                            p="15px"
+                            borderBottom="4px"
+                            borderColor={
+                                !recommend
+                                    ? "twitter.500"
+                                    : "rgba(255, 255, 255, 0)"
+                            }
+                            color={
+                                !recommend
+                                    ? "white"
+                                    : "rgba(255, 255, 255, 0.4)"
+                            }
+                            onClick={() => {
+                                setRecommend(false);
+                            }}
+                        >
+                            팔로우 중
+                        </Box>
+                    </Container>
+                </HStack>
+            </VStack>
+        </Box>
     );
 }
