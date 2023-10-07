b43b2842482a1050ca8b41e99d9b0c5db4ae0800
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-07 21:49:31 +0900

    created profile button

---
+++ twitter/src/routes/feed_style/sidebar_style/profile_button.tsx
@@ -0,0 +1,21 @@
+import { HStack, Text } from "@chakra-ui/react";
+import { FaRegUser } from "react-icons/fa6";
+
+export default function ProfileButton() {
+    return (
+        <HStack
+            fontSize="30px"
+            pl="10px"
+            pr="20px"
+            py="5px"
+            borderRadius="20px"
+            _hover={{ cursor: "pointer", bgColor: "whiteAlpha.300" }}
+            mb="10px"
+        >
+            <FaRegUser />
+            <Text fontSize="20px" fontWeight="bold" ml="10px">
+                프로필
+            </Text>
+        </HStack>
+    );
+}
