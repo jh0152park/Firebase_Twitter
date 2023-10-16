23e85b7c668f2ce979c0efce0c7be7d2f6e3be4d
박재현 <parkjaehyeon@bagjaehyeons-MacBook-Pro.local>
2023-10-16 15:39:54 +0900

    created profile page

--- twitter/src/routes/profile.tsx
+++ twitter/src/routes/profile.tsx
@@ -1,3 +1,34 @@
+import { Box, Center, HStack } from "@chakra-ui/react";
+import FeedBoard from "./feed_styles/feed_board";
+import Sidebar from "./feed_styles/sidebar";
+import { Helmet } from "react-helmet";
+import SuggestionCards from "./feed_styles/suggestion_cards";
+import GoToTop from "../components/go_to_top";
+import { auth } from "../firebase";
+
 export default function Profile() {
-    return null;
+    const user = auth.currentUser;
+    return (
+        <>
+            <Helmet>
+                <title>
+                    {`${user?.displayName} (@${user?.uid.slice(0.1)})`} / X
+                </title>
+            </Helmet>
+            <Center overflowY={"scroll"}>
+                <HStack>
+                    <Box id="sidebar" w="235px" position="fixed" top="0">
+                        <Sidebar></Sidebar>
+                    </Box>
+                    <Box id="feed_board" w="600px" ml="272px" mr="30px">
+                        {/* <FeedBoard></FeedBoard> */}
+                    </Box>
+                    <Box id="suggestion" w="350px" pt="10px">
+                        <SuggestionCards />
+                    </Box>
+                </HStack>
+            </Center>
+            <GoToTop />
+        </>
+    );
 }
